fc02b8e1dd93d43132d705f0abd5275f1fba2cb3
Sunghuii <gkemg2017@gmail.com>
2025-01-24 16:20:27 +0900

    #7 MemberController 수정

--- src/main/java/copy/project/demo/controller/MemberController.java
+++ src/main/java/copy/project/demo/controller/MemberController.java
@@ -18,25 +18,25 @@ public class MemberController {
     }
 
     /* 식별 값을 받아와서 회원 찾기*/
-    @GetMapping("/member/id/{id}")
+    @GetMapping("/id/{id}")
     public Member getMemberById(@PathVariable Long id){
         return memberService.findMemberById(id).orElseThrow(() -> new RuntimeException("회원을 찾을 수 없습니다."));
     }
 
     /* 이름으로 회원 찾기 */
-    @GetMapping("/member/name/{name}")
+    @GetMapping("/name/{name}")
     public Member getMemberByName(@PathVariable String name) {
         return memberService.findMemberByName(name).orElseThrow(() -> new RuntimeException("해당 이름의 회원이 없습니다."));
     }
 
     /* 닉네임으로 회원 찾기 */
-    @GetMapping("/member/nickname/{nickname}")
+    @GetMapping("/nickname/{nickname}")
     public Member getMemberByNickname(@PathVariable String nickname) {
         return memberService.findMemberByNickname(nickname).orElseThrow(() -> new RuntimeException("해당 닉네임의 회원이 없습니다"));
     }
 
     /* 휴대폰 번호로 회원 찾기 */
-    @GetMapping("/member/phone/{phone}")
+    @GetMapping("/phone/{phone}")
     public Member getMemberByPhone(@PathVariable String phone) {
         return memberService.findMemberByPhone(phone).orElseThrow(() -> new RuntimeException("해당 번호를 가진 회원이 없습니다."));
     }
