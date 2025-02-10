3308d4463144bef060cee39de211d102d28fe7e9
SungIn <sungin960406@gmail.com>
2025-02-10 23:02:12 +0900

    #20 헤더 및 메뉴 퍼블리싱 및 reset.css 추가

---
+++ src/main/front/src/components/Header/Sidebar.jsx
@@ -0,0 +1,48 @@
+import 'react';
+import MenuItems from './MenuItems';
+
+// sihong 20250210 - 사이드바 메뉴 컴포넌트 추가
+const Sidebar = () => {
+
+    const travelMenus = [
+        {id:"1", name:"국내숙소"},
+        {id:"2", name:"해외숙소"},
+        {id:"3", name:"항공"},
+        {id:"4", name:"항공+숙소"},
+        {id:"5", name:"레저·티켓"},
+        {id:"6", name:"렌터카"},
+        {id:"7", name:"공간대여"},
+    ];
+
+    const otherMenus = [
+        {id:"1", name:"비회원 예약조회"},
+        {id:"2", name:"이벤트"},
+        {id:"3", name:"고객센터"},
+    ]
+
+    return (
+        <aside>
+            <section className="side-menu__action-bar"></section>
+            <section className="side-menu__contents">
+                {/* 이미지 영역 */}
+                <div></div>
+                {/* 회원가입 / 로그인 버튼 */}
+                <div></div>
+                {/* 메뉴 리스트 */}
+                <div className="menu-group-wrapper">
+                    <span>모든 여행</span>
+                    <ul className='menu-items-wrapper'>
+                        {travelMenus.map(item => <MenuItems key={item.id} menuName={item.name} />)}
+                    </ul>
+                </div>
+                <div className="menu-group-wrapper">
+                    <ul>
+                        {otherMenus.map(item => <MenuItems key={item.id} menuName={item.name} />)}
+                    </ul>
+                </div>
+            </section>
+        </aside>
+    );
+}
+
+export default Sidebar;
