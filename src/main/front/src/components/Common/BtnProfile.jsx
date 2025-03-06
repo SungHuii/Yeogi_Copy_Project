ddd800866f2c49337f858c9b3ac1a70b11e3d8e9
SungIn <sungin960406@gmail.com>
2025-03-06 22:06:31 +0900

    #39 사이드 메뉴 퍼블리싱

--- src/main/front/src/components/Common/BtnProfile.jsx
+++ src/main/front/src/components/Common/BtnProfile.jsx
@@ -1,11 +1,11 @@
-import "react";
+import 'react';
 
-const BtnProfile = ({onClick}) => {
+const BtnProfile = ({ onClick }) => {
     return (
-        <button type="button" className="btn-login" onClick={onClick}>
+        <button type="button" className="btn login blue" onClick={onClick}>
             프로필 페이지로
         </button>
     );
-}
+};
 
 export default BtnProfile;
